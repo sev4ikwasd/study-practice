80ac097b4b54467b9f7aa6c626224012ff5e25a9
Vsevolod Bezaev <vvbezaev@gmail.com>
2022-07-11 13:54:04 +0300

    Add shutdown controller

---
+++ src/main/java/com/example/practice/controller/ShutdownController.java
@@ -0,0 +1,23 @@
+package com.example.practice.controller;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShutdownController implements ApplicationContextAware {
+    private ApplicationContext context;
+
+    @GetMapping("/shutdown")
+    public void shutdown() {
+        ((ConfigurableApplicationContext) context).close();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+}
