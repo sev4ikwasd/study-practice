7045fee720b0a85e96e835c34c3919a36eaf68ae
Vsevolod Bezaev <vvbezaev@gmail.com>
2022-07-14 18:21:58 +0300

    Change exceptions status codes

--- src/main/java/com/example/practice/exception/HeadingNotFoundException.java
+++ src/main/java/com/example/practice/exception/HeadingNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.practice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.UUID;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class HeadingNotFoundException extends RuntimeException {
     public HeadingNotFoundException(UUID id) {
         super("Heading with id " + id.toString() + " was not found");
--- src/main/java/com/example/practice/exception/MarketNotFoundException.java
+++ src/main/java/com/example/practice/exception/MarketNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.practice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.UUID;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MarketNotFoundException extends RuntimeException {
     public MarketNotFoundException(UUID id) {
         super("Market with id " + id.toString() + " was not found");
